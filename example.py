75bd720e13accb7bcf30a5ba696480bb7d539bdd
Enio Fernandes <EN10@users.noreply.github.com>
2022-07-20 13:56:22 +0100

    Create example.py

---
+++ example.py
@@ -0,0 +1,19 @@
+# pip3 install neo4j-driver
+# python3 example.py
+
+from neo4j import GraphDatabase, basic_auth
+
+driver = GraphDatabase.driver(
+  "neo4j+s://<DBHASH>.databases.neo4j.io:7687",
+  auth=basic_auth("<USERNAME>", "<PASSWORD>"))
+
+# Cypher query
+cypher_query = '''
+Match (m:Movie) where m.released > 2000 RETURN m limit 5
+'''
+
+with driver.session(database="neo4j") as session:
+    results = session.run(cypher_query).data()
+
+for record in results:
+    print(record['m']['title'])
